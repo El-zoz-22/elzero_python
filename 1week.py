11f42837c917b589e62b2cab02489a0e4c66e181
El-zoz-22 <143459406+El-zoz-22@users.noreply.github.com>
2024-04-12 19:26:38 +0200

    Add files via upload

---
+++ 1week.py
@@ -0,0 +1,22 @@
+#-----------------------------
+#week 1 in python 
+# ----------------------------
+
+Name= "ali"
+Age= "15"
+Country= "Egypt"
+
+print(" Name: ", Name ,"\n","Age: ", Age,"\n","Country: ", Country)
+
+print("\n",25*"*")
+#********************************
+
+print("Hello, my name is " + Name + " and i'm" + Age + " years old and i live in " + Country)
+
+print("\n",25*"*")
+#********************************
+
+print(type(Name),"\n",type(Age),"\n",type(Country))
+
+
+
